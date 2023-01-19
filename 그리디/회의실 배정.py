d857673081cb60b427f46113a37ac7d517f5e236
백채연 <codusl100@naver.com>
2023-01-19 15:55:49 +0900

    [23.01.19] 그리디 : 회의실 배정

--- 그리디/회의실 배정.py
+++ 그리디/회의실 배정.py
@@ -1,30 +1,61 @@
 # 230117
 # 백준 1931번
 
-N = int(input())
-arr = list()
-for i in range(N) :
-    arr.append(list(map(int, input().split())))
-arr.sort()
+# N = int(input())
+# arr = list()
+# for i in range(N) :
+#     arr.append(list(map(int, input().split())))
+# arr.sort()
 
-time = list()
+# time = list()
 
-start = 0
-fin = 0
+# min = arr[0]
+# num = 0
 
-for i in range(0, N-1) :
-    if len(time) == 0 :
-        time.append(arr[i])
-    # 0항이 기존보다 크고 끝나는 시각 더 적을 때
-    start = time[i][0]
-    fin = time[i][1]
-    else :
-        if arr[i][0] == time[-1][0] :
-            arr
-        if time[-1][1] + (time[-1][1] - time[-1][0]) >= arr[i][0] :
-            time.remove(time[-1])
-            time.append(arr[i])
-    # elif len(time) != 0 :
-    #     if time[-1][1] >=
+# for i in range(1, len(arr)) :
+#     if min[0] <= arr[i][0] and min[1] <= arr[i][1] :
+#         min = arr[i]
+#         time.append(min) 
+#     if min[0] <= arr[i][0] and arr[i][1] <= min[1] :
+#         min = arr[i]
+#         num = i
+#         time.append(min)
         
-print(time)
+# for i in range(num + 1, len(arr)) :
+#     if min[1] <= arr[i][0] :
+#         min = arr[i]
+#         time.append(min)
+#     elif min == arr[i] :
+#         time.append(arr[i])
+
+# print(time)
+# print(len(time))
+
+# 위에처럼 풀었을 때 예제의 답은 맞으나 회의시간의 시작, 끝나는 시간이 같을 때 계속 틀렸음
+# 문제를 풀면서 고려해야하는 점은 빨리 끝나는 순으로 정렬해야 한다
+# 그리고 끝나는 시간이 같다면 빨리 시작하는 순으로 정렬되어야 한다
+# 문제풀이 방법
+# 1. 전체를 시작시간의 오름차순으로 정렬한 후 정렬된 리스트를 다시 끝나는 시간대로 정렬하기
+# => 이미 시작시간 오름차순으로 정렬되어 있기에 끝나는 시간이 같을 때 시작 시간의 오름차순으로 정렬됨
+# 2. 한번에 정렬 (Key에 튜플로 여러 인자를 주면 해당 인자의 순서대로 정렬을 해준다)
+
+## 2차원 요소를 기준으로 정렬할 때 sorted(list, key = lambda a:a[n]) 사용
+
+n = int(input())
+room = []
+
+for i in range(n):
+    a, b = map(int, input().split())
+    room.append([a, b])
+
+room.sort(key = lambda x: x[0])
+room.sort(key = lambda x: x[1])
+
+cnt = 1
+end = room[0][1]
+for i in range(1, n):
+    if room[i][0] >= end:
+        cnt += 1
+        end = room[i][1]
+
+print(cnt)
