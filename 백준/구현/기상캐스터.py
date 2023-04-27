3b332e916e40a53fb9a8ab65d449e9c1a6189803
unknown <codusl100@naver.com>
2023-04-27 15:41:18 +0900

    구현 : 기상캐스터.py 주석처리

--- 백준/구현/기상캐스터.py
+++ 백준/구현/기상캐스터.py
@@ -1,11 +1,11 @@
-H, W = map(int, input().split())
-sky = [input() for _ in range(H)]
-answer = [[0] * W for _ in range(H)]
+# H, W = map(int, input().split())
+# sky = [input() for _ in range(H)]
+# answer = [[0] * W for _ in range(H)]
 
-for i in range(H):
-    ans = list()
-    if 'c' not in sky:
-        print(*i, end=' ')
-        print()
-    else: # c 한번이라도 있으면
-        for j in range(W):
+# for i in range(H):
+#     ans = list()
+#     if 'c' not in sky:
+#         print(*i, end=' ')
+#         print()
+#     else: # c 한번이라도 있으면
+#         for j in range(W):
