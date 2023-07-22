dbf4a629dd9a2b44765ea9495217896d9c69abb5
codusl100 <codusl100@naver.com>
2023-07-22 10:49:47 +0900

    230722 DFS&BFS : DFS와 BFS

---
+++ 백준/DFS&BFS/DFS와 BFS.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+dfs_list = [V]
+bfs_list = [V]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a] += [b]
+    graph[b] += [a]
+for i in range(len(graph)):
+    graph[i].sort()
+
+def dfs(v):
+    visited[v] = 1
+    for i in graph[v]:
+        if visited[i] == 0: # 방문 안했으면
+            dfs_list.append(i)
+            dfs(i)
+def bfs(graph, V, visited):
+    queue = deque([V])
+    visited[V] = 1
+    while queue:
+        q = queue.popleft()
+        for i in graph[q]:
+            if visited[i] == 0: # 방문 안했으면
+                visited[i] = 1
+                bfs_list.append(i)
+                queue.append(i)
+dfs(V) 
+visited = [0] * (N+1)
+bfs(graph, V, visited)
+
+print(' '.join(str(item) for item in dfs_list))
+print(' '.join(str(item) for item in bfs_list))
