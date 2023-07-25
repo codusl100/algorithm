7ee9c1d9447bf2fdf612ed79d4a7b8a940481f29
codusl100 <codusl100@naver.com>
2023-07-25 22:55:25 +0900

    230725 DFS&BFS : 안전 영역(BFS)

---
+++ 백준/DFS&BFS/안전 영역(BFS).py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(10**7)
+
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+visited = [0] * N
+max_height = max(map(max, graph))
+
+result = 0
+ans = 1
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def bfs(a, b, height):
+    queue = deque([(a, b)])
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (0 <= nx < N) and (0 <= ny < N) and not visited[nx][ny] and graph[nx][ny] > height:
+                visited[nx][ny] = True
+                queue.append((nx, ny)) 
+    
+for k in range(1, max_height+1):
+    visited = [[False]*N for _ in range(N)]
+    result = 0
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] > k and not visited[i][j]: 
+                result += 1
+                visited[i][j] = True
+                bfs(i, j, k)
+    ans = max(ans, result)
+
+print(ans)
