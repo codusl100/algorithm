433736e867c517853c303b903c61c1d58b7b3df1
codus <codusl100@naver.com>
2023-11-18 13:23:48 +0900

    [23.11.18] DFS: 반복수열

--- 백준/DFS&BFS/반복수열.py
+++ 백준/DFS&BFS/반복수열.py
@@ -1,19 +1,21 @@
 A, P = map(int, input().split()) # 57, 2
-N = len(str(A)) - 1
-B = A
-graph = [[]]
+visited = [0]*250000 # 최댓값 4*9^5
+count = 1
 
-# A를 자릿수대로 나눈 후 배열 [5, 7] 생성해서 graph 안에 넣음
-print(N)
-while N > 0:
-    B = A // (10 ** N) # 몫
-    graph[0].append(B)
-    A %= 10 ** N # 나머지
-    N -= 1
-graph[0].append(A)
+def square(a, p):
+    ans = 0
+    for i in str(a):
+        ans += pow(int(i), p)
+    return ans
 
-def dfs(v):
-    graph[v+1] = []
-    for i in range(len(str(A))):
-        graph[v+1].append(graph[v][i] ** P)
-        
+def dfs(v, p, count, visited):
+    if visited[v] != 0:
+        return visited[v] -1
+    visited[v] = count
+    next = square(v, p)
+    count += 1
+
+    return dfs(next, p, count, visited)
+
+
+print(dfs(A, P, count, visited))
