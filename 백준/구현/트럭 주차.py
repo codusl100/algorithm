3bbe90af39994fd095310290251ee71e436358a2
codus <codusl100@naver.com>
2023-11-20 15:04:48 +0900

    [23.11.20] 구현: 트럭 주차

---
+++ 백준/구현/트럭 주차.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+A, B, C = map(int, input().split())
+car = [0] * 100
+for i in range(3):
+    in_, out_ = map(int, input().split())
+    for j in range(in_, out_):
+        car[j] += 1
+ans = 0
+print(car)
+for i in car:
+    if i == 1:
+        ans += A * i
+    elif i == 2:
+        ans += B * i
+    elif i == 3:
+        ans += C * i
+print(ans)
