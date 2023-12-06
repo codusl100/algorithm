57b20ebd83cdff493b5e2367cf45c876430af2ce
codus <codusl100@naver.com>
2023-12-06 11:46:58 +0900

    [23.12.06] 동적계획법: 병사 배치하기

---
+++ 백준/동적계획법/병사 배치하기.py
@@ -0,0 +1,10 @@
+N = int(input())
+warriors = list(map(int, input().split()))
+dp = [1] * N
+
+for i in range(N):
+    for j in range(i):
+        if warriors[i] < warriors[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(dp)
