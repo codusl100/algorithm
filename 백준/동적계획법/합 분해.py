757e434916e49cb04dc8fd228f7315386eea9941
codus <codusl100@naver.com>
2024-01-04 17:08:42 +0900

    [24.01.04] 동적계획법: 합 분해

--- 백준/동적계획법/합 분해.py
+++ 백준/동적계획법/합 분해.py
@@ -1,13 +1,13 @@
 N, K = map(int, input().split())
-dp = [0] * N
-dp[K] = 1
-k = 1
-print(dp)
+dp = [[0] * 201 for i in range(201)]
 
-for i in range(K+1, N):
-    print("i:" + str(i))
-    dp[i] = dp[i-1] + k
-    k += 1
-    print(dp)
+for i in range(201):
+    dp[1][i] = 1
+    dp[2][i] = i+1
 
-print(dp[N-1] % 1000000000)
+for i in range(2, 201):
+    dp[i][1] = i
+    for j in range(i, 201):
+        dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000000000
+
+print(dp[K][N])
