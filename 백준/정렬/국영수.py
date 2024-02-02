be70f544e52e730b06110c58223aa8d46d99cb49
chaeyeon <codusl100@naver.com>
2024-02-02 12:58:07 +0900

    [24.02.02] 정렬: 국영수

---
+++ 백준/정렬/국영수.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = []
+for i in range(n):
+    name, x, y, z = input().split()
+    x = int(x)
+    y = int(y)
+    z = int(z)
+    arr.append([name, x, y, z])
+arr.sort(key=lambda a: (-a[1], a[2], a[3], a[0]))
+for i in range(n):
+    print(arr[i][0])
