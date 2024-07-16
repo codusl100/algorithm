7a34e8c9f13673b6841bd724eb53aa063b5784e2
chaeyeon <codusl100@naver.com>
2024-07-16 10:01:03 +0900

    [24.07.16] 백트래킹: N과 M (7)

---
+++ 백준/백트래킹/N과 M (7).py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+ans = []
+def recur():
+    if len(ans) == M:
+        print(*ans)
+        return
+    for i in arr:
+        ans.append(i)
+        recur()
+        ans.pop()
+recur()
